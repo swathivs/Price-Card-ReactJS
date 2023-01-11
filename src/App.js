4e87c18c9e586115f2aa28eabcc049804a2360e0
swathivs <swathivs1406@gmail.com>
2023-01-11 18:08:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Card1 from './Card1.js';
+import Card2 from './Card2.js';
+import Card3 from './Card3.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container d-flex justify-content-center gap-4 flex-wrap flex-row p-3 bg-color">
+      <Card1 />
+      <Card2 />
+      <Card3 />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card3.js
@@ -0,0 +1,79 @@
+import React from "react";
+const details=[
+    {
+      check:"✅",
+      data:<b>Unlimited Users</b>,
+  },{
+    check:"✅",
+    data:"150GB Storage",
+  },{
+    check:"✅",
+    data:"Unlimited Public Projects"
+  },{
+    check:"✅",
+    data:"Community Access"
+  },{
+    check:"✅",
+    data:"Unlimited Private Projects"
+  },{
+    check:"✅",
+    data:"Dedicated Phone Support"
+  },{
+    check:"✅",
+    data:"Unlimited Free Subdomain",
+  },{
+    check:"✅",
+    data:"Monthly Status Reports"
+  }
+  ]
+function Card3(){
+    return(
+        <div>
+      <div className="container bg-light card p-4 mx-auto rounded-3" >
+            <div className="text-muted text-center"> FREE</div>
+            <div className="text-center "><h1>$49<span className="month">/per month</span></h1></div>     
+            <hr></hr>
+        
+        <div>
+          <div>
+            <ul> 
+                    {details.map((x)=>{return <Tick check={x.check} data={x.data}/>
+                        
+                    })}
+                    </ul>
+                    
+                </div>
+                <div className="row"><input type="button" value="Subscribe" class="btn btn-primary fw-bold" ></input>
+                </div>
+                </div> 
+            </div>
+
+        </div>
+       
+    );
+    function Tick({check,data}){
+            if(check==="✅")
+            return(
+                <DisplayBold check={check} data={data}/>
+            );
+            else
+            return(
+                <DisplayMuted check={check} data={data}/>
+            );
+        
+    }
+    function DisplayBold({check,data}){
+        return(
+            
+            <li className="fw-semibold content">{check} {data}</li>
+        );
+    }
+    function DisplayMuted({check,data}){
+        return(
+            <div>
+                <li className="text-muted content">{check} {data}</li>
+            </div>
+        );
+    }
+}
+export default Card3;
